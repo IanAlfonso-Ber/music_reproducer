29488a818517eca9831c5bf99270b06f18be0ef1
Ian Daniel Alfonso Rodriguez <iandanielalfon@gmail.com>
2025-06-11 08:03:04 +0200

    Add files via upload

---
+++ bola/src/appinventor/ai_iandanielalfon/bola/Screen6.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"232","Source":"Form","Properties":{"$Name":"Screen6","$Type":"Form","$Version":"31","AccentColor":"&HFF0000FF","AppName":"bola","Theme":"Classic","Title":"Screen6","Uuid":"0"}}
+|#
